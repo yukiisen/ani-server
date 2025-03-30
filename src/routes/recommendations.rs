47330e370e1d2b3b2d723350edf961fa67398efc
yukiisen <ahmad.redhorn@gmail.com>
2025-03-30 15:37:15 +0100

    recommendations impl

--- src/routes/recommendations.rs
+++ src/routes/recommendations.rs
@@ -1,3 +1,24 @@
-pub async fn anime_recommendations () {
+use actix_web::web;
+use actix_web::Responder;
+use actix_web::HttpResponse;
 
+use ani_core::database::queries;
+use queries::get_anime_relations;
+
+use sqlx::SqlitePool;
+use log::error;
+
+
+pub async fn anime_recommendations (mal_id: web::Path<u32>, pool: web::Data<SqlitePool>) {
+    let recommended = get_anime_relations(*mal_id, &pool).await;
+
+    match recommended {
+        Ok(recs) => {
+            HttpResponse::Ok().json(recs)
+        },
+        Err(error) => {
+            error!(target: "Database Error", "{error}");
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
