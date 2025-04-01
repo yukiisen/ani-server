15f53e099fb3bde9695696f4d24ead0424e22750
yukiisen <ahmad.redhorn@gmail.com>
2025-04-01 15:01:29 +0100

    recommendations route implementation

--- src/routes/recommendations.rs
+++ src/routes/recommendations.rs
@@ -1,16 +1,15 @@
 use actix_web::web;
-use actix_web::Responder;
 use actix_web::HttpResponse;
 
 use ani_core::database::queries;
-use queries::get_anime_relations;
+use queries::get_anime_recommendations;
 
 use sqlx::SqlitePool;
 use log::error;
 
 
-pub async fn anime_recommendations (mal_id: web::Path<u32>, pool: web::Data<SqlitePool>) {
-    let recommended = get_anime_relations(*mal_id, &pool).await;
+pub async fn anime_recommendations (mal_id: web::Path<u32>, pool: web::Data<SqlitePool>) -> HttpResponse {
+    let recommended = get_anime_recommendations(*mal_id, &pool).await;
 
     match recommended {
         Ok(recs) => {
